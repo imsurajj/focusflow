b65d1de073ea55eb545d45e52577c43581fbc57f
Your Name <your.email@example.com>
2025-04-30 08:59:59 +0530

    Add : Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Courier_Prime } from 'next/font/google';
 import "./globals.css";
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 // Initialize the Courier Prime font
 const courierPrime = Courier_Prime({
@@ -34,6 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={courierPrime.className}>
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
